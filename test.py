ffca3a6d6cb0f2792ba9c7682ca825a556bc1d03
guptabhinav49 <guptabhinav49@gmail.com>
2021-03-04 21:08:52 +0530

    added testcases

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import time
 t1 = time.time()
 fin_eds=[]
 fin_st=[]
-for cc in range(1):
+for cc in range(10):
 	edges = []
 	with open("./testcases_dis/edges_{}.txt".format(cc)) as f:
 		for line in f:
@@ -18,7 +18,7 @@ for cc in range(1):
 	fin_st.append(s_t)
 	fin_eds.append(edges)
 counter=0
-for cs in range(1):
+for cs in range(10):
 	with open('./testcases_dis/resm_{}.txt'.format(cs) , 'w') as the_file:
 		for s,t in fin_st[cs]:
 			the_file.write('{}\n'.format(disconnect.find_minimal(fin_eds[cs],s,t)))
